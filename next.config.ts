95d22a36fda8aa8146419e0d5348a4d734dbc31d
Ayush Shekhawat <157112303+ayush7787@users.noreply.github.com>
2025-06-20 20:41:40 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export', // 🔑 this enables static site export
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
